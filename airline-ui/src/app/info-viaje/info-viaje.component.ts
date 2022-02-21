b0de389f8b69d8b0312dd0e7bf021925c1468448
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-02-21 14:31:43 -0500

    Creación de parte de interfaz para la captura de datos iniciales: ciudades, personas, fechas y tipoVuelo

--- airline-ui/src/app/info-viaje/info-viaje.component.ts
+++ airline-ui/src/app/info-viaje/info-viaje.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { RouteService } from '../services/route.service';
 
@@ -13,8 +14,14 @@ export class InfoViajeComponent implements OnInit {
   public allDestinationCities: any[] = []
   public selectedOrigin: string = ""
   public selectedDestination: string = ""
+  public adultNumber: number = 0
+  public childNumber: number = 0
+  public infantNumber: number = 0
+  public departureDate: Date = new Date()
+  public returnDate: Date = new Date()
 
   constructor(
+    public modal: NgbModal,
     private routeService: RouteService
   ) { }
 
@@ -42,5 +49,25 @@ export class InfoViajeComponent implements OnInit {
   // Obtiene el valor (ciudad de destino) seleccionada en el selected
   onSelecteddestination(e:any) {
     this.selectedDestination = e.target.value;
+  }
+
+  // Permite que el modal se abra o se muestre en pantalla
+  openNumberPassengers(passengers: any) {
+    this.modal.open(passengers);
+  }
+
+  // Permite obtener o capturar los valores de la cantidad de pasajeros ingresados de acuerdo al tipo
+  passengerSelection() {
+    this.adultNumber = Number((document.getElementById("numAdult") as HTMLInputElement).value);
+    this.childNumber = Number((document.getElementById("numChild") as HTMLInputElement).value);
+    this.infantNumber = Number((document.getElementById("numInfant") as HTMLInputElement).value);
+  }
+  
+  // Permite capturar los valores de las fechas ingresadas por el usuario
+  dateSelection() {
+    this.departureDate = new Date((document.getElementById("departureDate") as HTMLInputElement).value + '');
+    this.returnDate = new Date((document.getElementById("returnDate") as HTMLInputElement).value + '');
+    console.log(this.departureDate);
+    console.log(this.returnDate);    
   }
 }
