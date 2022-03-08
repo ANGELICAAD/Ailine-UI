c2be6a500e968c9c526eb6eb6e474825cce64f98
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-03-08 10:14:23 -0500

    Nueva configuración

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 import { City } from '../interfaces/city';
 
@@ -8,13 +9,16 @@ import { City } from '../interfaces/city';
 })
 export class CityService {
 
+  private apiServerURL = environment.apiBaseURL
+
   constructor(
     private http: HttpClient
   ) { }
 
   // Método que permite validar si una ciudad destino requiere de visa
   getVisaRequired(name: string) {
-    const path = `http://localhost:8080/api/city/${name}`;
+    // const path = `http://localhost:8080/api/city/${name}`;
+    const path = `${this.apiServerURL}/api/city/${name}`;
     return this.http.get<boolean>(path);
   }
 }
