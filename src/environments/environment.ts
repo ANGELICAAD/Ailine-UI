6abd13232da3f0874b2aca68c01809a0c3c234b6
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-04-30 09:33:10 -0500

    Actualización de configuración

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseURL: 'https://spring-boot-airline.herokuapp.com/'
+  apiBaseURL: 'https://spring-boot-airline.herokuapp.com/api'
 };
 
 /*
