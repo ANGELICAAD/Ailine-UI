b7e33e00c2d7e1aee6b37f96cfa2e30c53105629
angelica.arroyave <angelica.arroyave@landsoft.com.co>
2022-05-07 14:53:36 -0500

    Cambio de conexión de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiBaseURL: 'https://spring-boot-airline.herokuapp.com'
-  apiBaseURL: 'http://localhost:8080/'
+  apiBaseURL: 'https://spring-boot-airline.herokuapp.com'
+  // apiBaseURL: 'http://localhost:8080/'
 };
 
 /*
